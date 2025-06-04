25006f498be2d0c052991cf8bf4be5beca9a586d
vladimirk7 <kuklin1201@gmai.com>
2025-06-04 16:19:17 +0300

    можно переместиться к 1й секции

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+document.getElementById('link-to-section1').addEventListener('click', function(event) {
+  event.preventDefault(); 
+
+  const targetId = this.getAttribute('href').substring(1); 
+  const targetElement = document.getElementById(targetId);
+
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  }
+});
