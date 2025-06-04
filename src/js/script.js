8d969dca4406905f7829fd832d4bb915ae93eccb
vladimirk7 <kuklin1201@gmai.com>
2025-06-04 16:58:03 +0300

    исправил ошибку с названием id для Landing и можно перемещатся по 2й ссылке

--- src/js/script.js
+++ src/js/script.js
@@ -8,3 +8,14 @@ document.getElementById('link-to-section1').addEventListener('click', function(e
     targetElement.scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+document.getElementById('link-to-section2').addEventListener('click', function(event) {
+  event.preventDefault(); 
+
+  const targetId = this.getAttribute('href').substring(1); 
+  const targetElement = document.getElementById(targetId);
+
+  if (targetElement) {
+    targetElement.scrollIntoView({ behavior: 'smooth' });
+  }
+});
